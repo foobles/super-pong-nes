4f14405797b0321c05aa356fe93400e6e7df7241
April <buzdav2@gmail.com>
2022-03-23 00:56:59 -0500

    actor_inc.s now imports collision arrays

--- src/actors_inc.s
+++ src/actors_inc.s
@@ -1,5 +1,6 @@
 .globalzp   temp
 .importzp   actor_flags, actor_ids, actor_xs, actor_ys, actor_updaters_lo, actor_updaters_hi
+.import     actor_collisions_x, actor_collisions_y, actor_collisions_w, actor_collisions_h
 .import     actor_data0, actor_data1, actor_data2, actor_data3
 .importzp   actor_next_idx, actor_count
 
